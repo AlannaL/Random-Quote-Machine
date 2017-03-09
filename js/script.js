8e1be2eb5c56ac43b2da9cb4b9ac7a84a31ab3b4
Alanna Lynch <alanna.copland.lynch@gmail.com>
2017-03-09 21:48:33 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function() {
+  //set global variables
+  var quote = "";
+  var author = "";
+
+  //display intial quote
+  $.getJSON("https://quote-api.gomix.me/pull/1", function(data) {
+    quote = data[0].body;
+    author = data[0].author;
+    $('#quote-body').text('"' + quote + '"');
+    $('#name').text("-- " + author);
+  });
+
+  //display quote when button is clicked
+  $("#quote").on("click", function() {
+    $.getJSON("https://quote-api.gomix.me/pull/1", function(data) {
+      quote = data[0].body;
+      author = data[0].author;
+      $('#quote-body').text('"' + quote + '"');
+      $('#name').text("-- " + author);
+    });
+  });
+  
+  //tweet out quote on click
+  $(".twitter-share-button").on("click", function() {
+      $('a').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" -- ' +
+        author +''));
+    })
+ 
+})
+
+// version with ajax 
+ /* 
+$.ajax({
+  url: "https://quote-api.gomix.me/pull/1",
+  type: 'GET',
+  data: {},
+  dataType: 'json',
+  success: function(data){
+    quote = data[0].body;
+    author = data[0].author;
+    $('#quote-body').text('"' + quote + '"');
+    $('#name').text("--" + author);
+  },
+  error: function(error) { 
+    alert(err); 
+   }
+  }); */
